c2da2a6ec5ba1f38b56e863f717794308537368c
zhaohuan <1987287033@qq.com>
2017-11-11 22:18:53 +0800

    backEnd add some function

--- backEnd/js/productManage.js
+++ backEnd/js/productManage.js
@@ -12,23 +12,7 @@ $(function () {
 
                 $('tbody').html(productResult);
 
-                // 分页
-                $('.pagination').bootstrapPaginator({
-                    // 当前使用的是3版本的bootstrap
-                    bootstrapMajorVersion: 3,
-                    // 配置的字体大小是小号
-                    size: 'small',
-                    // 当前页
-                    currentPage: data.page,
-                    // 一共多少页
-                    // 总页数=数据的总数/每页显示多少条数据
-                    totalPages: Math.ceil(data.total / data.size),
-                    // 点击页面事件
-                    onPageClicked: function (event, originalEvent, type, page) {
-                        //    改变当前页面渲染 page 当前点击的按钮的页面
-                        getProductData(data);
-                    }
-                });
+               
             }
         })
     }
